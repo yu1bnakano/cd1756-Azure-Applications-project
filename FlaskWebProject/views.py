0f8bccc729400f0fddff9ff6df1c02bb61625d6f
yu1bnakano <nakano.yuiichi@gmail.com>
2025-05-29 00:23:53 +0800

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -115,12 +115,15 @@ def logout():
 
 def _load_cache():
     # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get('token_cache'):
+        cache.deserialize(session['token_cache'])
     return cache
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
-    pass
+    if cache.has_state_changed:
+        session['token_cache'] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
     # TODO: Return a ConfidentialClientApplication
